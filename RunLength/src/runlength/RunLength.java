4878740f77d5279d06f32f828112d58c9faf3300
coomkie <153831957+coomkie@users.noreply.github.com>
2024-03-27 11:06:32 +0700

    done runlength

--- RunLength/src/runlength/RunLength.java
+++ RunLength/src/runlength/RunLength.java
@@ -12,14 +12,28 @@ import java.util.Scanner;
  * character and return the count + character
  */
 public class RunLength {
-    
+
     public static String runLength(String str) { //helloo worlf -> he2l2o worlf
-        return "1";
+        StringBuilder rs = new StringBuilder();
+        int count = 1;
+        for (int i = 0; i < str.length(); i++) {
+            if (i + 1 < str.length() && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                if (count == 1) {
+                    rs.append(str.charAt(i));
+                } else {
+                    rs.append(count).append(str.charAt(i));
+                    count = 1;
+                }
+            }
+        }
+        return rs.toString();
     }
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(runLength(sc.nextLine()));
     }
-    
+
 }
